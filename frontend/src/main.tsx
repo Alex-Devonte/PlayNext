106bf716937b931ca355e354b39be406a2b77af6
Alex <alex95.taylor@gmail.com>
2025-06-26 10:08:13 -0400

    Remove unused import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: import.meta.env.VITE_GRAPHQL_URL,
