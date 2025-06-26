524d659473c42fd20f4961ff74fd87e2229d677d
Alex <alex95.taylor@gmail.com>
2025-06-26 10:06:45 -0400

    Add query for game detail using game ID

--- frontend/src/components/GameDetail.tsx
+++ frontend/src/components/GameDetail.tsx
@@ -1,8 +1,80 @@
+import { gql, useQuery } from "@apollo/client";
 import { useLocation } from "react-router-dom";
 
 function GameDetail() {
+  // Grab the game ID from the location state
   const location = useLocation();
-  console.log("GameDetail location state:", location.state.id);
+  const id = parseInt(location.state.id, 10);
+
+  const DETAIL_QUERY = gql`
+    query GameDetail($id: ID!) {
+      gameDetail(id: $id) {
+        id
+        name
+        summary
+        genres {
+          id
+          name
+        }
+        ageRatings {
+          id
+          ratingCategory {
+            id
+            rating
+          }
+          organization {
+            id
+            name
+          }
+        }
+        cover {
+          url
+        }
+        releaseDates {
+          id
+          human
+        }
+        keywords {
+          id
+          name
+        }
+        developers {
+          id
+          name
+        }
+        publishers {
+          id
+          name
+        }
+        platforms {
+          id
+          name
+        }
+        perspectives {
+          id
+          name
+        }
+        gameModes {
+          id
+          name
+        }
+        timeToBeat {
+          hastily
+          normally
+          completely
+        }
+      }
+    }
+  `;
+
+  const { loading, error, data } = useQuery(DETAIL_QUERY, {
+    variables: { id: id },
+  });
+
+  if (loading) return <p>Loading game details...</p>;
+  if (error) return <p>Error fetching game details: {error.message}</p>;
+  console.log("Game detail data:", data);
+
   return (
     <div>
       <h1>Game Detail</h1>
