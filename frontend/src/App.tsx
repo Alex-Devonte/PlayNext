b481d91a2468c8e8917d318ff2e4da88ed274999
Alex <alex95.taylor@gmail.com>
2025-07-06 11:35:14 -0400

    Add route for the results page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from "./components/HomePage";
 import Footer from "./components/Footer";
 import GameDetail from "./components/GameDetail";
 import Questionnaire from "./components/Questionnaire";
+import Results from "./components/Results";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/questionnaire" element={<Questionnaire />} />
         <Route path="/game/:id" element={<GameDetail />} />
+        <Route path="/results" element={<Results />} />
       </Routes>
       <Footer />
     </Router>
