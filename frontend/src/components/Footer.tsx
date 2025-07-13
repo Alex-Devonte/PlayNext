82ed73fa2898ce0f5d1a4688881db1b6c869bc9a
Alex <alex95.taylor@gmail.com>
2025-07-13 18:30:34 -0400

    Remove fixed positioning

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="fixed bottom-0 w-full bg-gray-800 p-2 text-center text-white">
+    <footer className="bottom-0 w-full bg-gray-800 p-2 text-center text-white">
       <p>Alex Taylor 2025</p>
     </footer>
   );
