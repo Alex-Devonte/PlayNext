b2e8f42634e9513b428fbdc72ab55548c338fece
Alex <alex95.taylor@gmail.com>
2025-07-13 19:20:45 -0400

    Remove positioning from footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <footer className="bottom-0 w-full bg-gray-800 p-2 text-center text-white">
+    <footer className="w-full bg-gray-800 p-2 text-center text-white">
       <p>Alex Taylor 2025</p>
     </footer>
   );
