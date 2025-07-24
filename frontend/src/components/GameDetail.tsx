7185152a96dd9116791a47ff21b3ab2ac894ddf8
Alex <alex95.taylor@gmail.com>
2025-07-24 19:10:44 -0400

    Update game detail styles for larger screens

--- frontend/src/components/GameDetail.tsx
+++ frontend/src/components/GameDetail.tsx
@@ -119,116 +119,110 @@ function GameDetail() {
   if (!game) return <p>No game details found.</p>;
 
   return (
-    <div className="text-dark mx-auto mb-10 px-5 py-10 lg:px-16 xl:max-w-3/5">
-      <div className="flex flex-col gap-8 lg:flex-row">
-        <div className="mx-auto w-full max-w-md lg:w-1/3">
-          <img
-            src={game.cover.url.replace("t_thumb", "t_cover_big")}
-            alt={`${game.name} cover`}
-            className="h-auto w-full rounded-lg object-cover"
-          />
-        </div>
-
-        <div className="flex-1">
-          <h1 className="text-primary mb-5 text-4xl font-bold md:text-5xl lg:text-6xl">
-            {game.name}
-          </h1>
-          <p className="mb-5">{game.summary}</p>
-
-          <div className="flex flex-wrap gap-4">
-            <p>
-              <span className="font-bold">Genres:</span>{" "}
-              <span className="text-primary font-semibold">
-                {game.genres.map((genre) => genre.name).join(", ")}
-              </span>
-            </p>
-            <p>
-              <span className="font-bold">Platforms:</span>{" "}
-              <span className="text-primary font-semibold">
-                {game.platforms.map((platform) => platform.name).join(", ")}
-              </span>
-            </p>
-            <p>
-              <span className="font-bold">Release Date:</span>{" "}
-              <span className="text-primary font-semibold">
-                {game.releaseDates[0]?.human}
-              </span>
-            </p>
+    <div className="bg-light">
+      <div className="text-dark mb-10 px-5 py-10 lg:mx-auto lg:mt-10 lg:max-w-5xl lg:rounded-xl lg:border lg:border-gray-300 lg:bg-white lg:px-16 lg:shadow-2xl">
+        <div className="flex flex-col gap-8 lg:flex-row">
+          <div className="mx-auto w-full max-w-md lg:w-1/3">
+            <img
+              src={game.cover.url.replace("t_thumb", "t_cover_big")}
+              alt={`${game.name} cover`}
+              className="h-auto w-full rounded-lg object-cover"
+            />
+          </div>
+          <div className="flex-1">
+            <h1 className="text-primary mb-5 text-4xl font-bold md:text-5xl lg:text-6xl">
+              {game.name}
+            </h1>
+            <p className="mb-5">{game.summary}</p>
+            <div className="flex flex-wrap gap-4">
+              <p>
+                <span className="font-bold">Genres:</span>{" "}
+                <span className="text-primary font-semibold">
+                  {game.genres.map((genre) => genre.name).join(", ")}
+                </span>
+              </p>
+              <p>
+                <span className="font-bold">Platforms:</span>{" "}
+                <span className="text-primary font-semibold">
+                  {game.platforms.map((platform) => platform.name).join(", ")}
+                </span>
+              </p>
+              <p>
+                <span className="font-bold">Release Date:</span>{" "}
+                <span className="text-primary font-semibold">
+                  {game.releaseDates[0]?.human}
+                </span>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div className="mt-10 grid gap-10 text-sm md:grid-cols-2 lg:grid-cols-3">
-        <div>
-          <h2 className="mb-1 text-lg font-bold">Developer</h2>
-          <p className="text-primary font-semibold">
-            {game.developers?.length > 0
-              ? game.developers.map((developer) => developer.name).join(", ")
-              : "N/A"}
-          </p>
-        </div>
-
-        <div>
-          <h2 className="mb-1 text-lg font-bold">Publisher</h2>
-          <p className="text-primary font-semibold">
-            {game.publishers?.length > 0
-              ? game.publishers.map((publisher) => publisher.name).join(", ")
-              : "N/A"}
-          </p>
-        </div>
-
-        <div>
-          <h2 className="mb-1 text-lg font-bold">Keywords</h2>
-          <p className="text-primary italic">
-            {overKeywordLimit && !showMore
-              ? `${keywordsText.substring(0, KEYWORD_LIMIT)}...`
-              : keywordsText}
-          </p>
-          {overKeywordLimit && (
-            <p
-              className="text-secondary mt-1 cursor-pointer font-bold hover:underline"
-              onClick={() => setShowMore((prev) => !prev)}
-            >
-              {showMore ? "Show less" : "Show more"}
+        <div className="mt-10 grid gap-10 text-sm md:grid-cols-2 lg:grid-cols-3">
+          <div>
+            <h2 className="mb-1 text-lg font-bold">Developer</h2>
+            <p className="text-primary font-semibold">
+              {game.developers?.length > 0
+                ? game.developers.map((developer) => developer.name).join(", ")
+                : "N/A"}
             </p>
-          )}
+          </div>
+          <div>
+            <h2 className="mb-1 text-lg font-bold">Publisher</h2>
+            <p className="text-primary font-semibold">
+              {game.publishers?.length > 0
+                ? game.publishers.map((publisher) => publisher.name).join(", ")
+                : "N/A"}
+            </p>
+          </div>
+          <div>
+            <h2 className="mb-1 text-lg font-bold">Keywords</h2>
+            <p className="text-primary italic">
+              {overKeywordLimit && !showMore
+                ? `${keywordsText.substring(0, KEYWORD_LIMIT)}...`
+                : keywordsText}
+            </p>
+            {overKeywordLimit && (
+              <p
+                className="text-secondary mt-1 cursor-pointer font-bold hover:underline"
+                onClick={() => setShowMore((prev) => !prev)}
+              >
+                {showMore ? "Show less" : "Show more"}
+              </p>
+            )}
+          </div>
+          <div>
+            <h2 className="mb-1 text-lg font-bold">Perspective</h2>
+            <p className="text-primary font-semibold">
+              {game.perspectives?.length > 0
+                ? game.perspectives
+                    .map((perspective) => perspective.name)
+                    .join(", ")
+                : "N/A"}
+            </p>
+          </div>
+          <div>
+            <h2 className="mb-1 text-lg font-bold">Game Modes</h2>
+            <p className="text-primary font-semibold">
+              {game.gameModes?.length > 0
+                ? game.gameModes.map((mode) => mode.name).join(", ")
+                : "N/A"}
+            </p>
+          </div>
+          <div className="mb-5">
+            <h2 className="mb-1 text-lg font-semibold">Age Ratings</h2>
+            <ul className="grid grid-cols-3">
+              {game.ageRatings?.length > 0
+                ? game.ageRatings.map((rating) => (
+                    <li className="text-dark font-semibold" key={rating.id}>
+                      {rating.organization.name}
+                    </li>
+                  ))
+                : "N/A"}
+            </ul>
+          </div>
         </div>
-
-        <div>
-          <h2 className="mb-1 text-lg font-bold">Perspective</h2>
-          <p className="text-primary font-semibold">
-            {game.perspectives?.length > 0
-              ? game.perspectives
-                  .map((perspective) => perspective.name)
-                  .join(", ")
-              : "N/A"}
-          </p>
+        <div className="mt-5 flex items-center justify-center lg:hidden">
+          <a href="#">Back to Top</a>
         </div>
-
-        <div>
-          <h2 className="mb-1 text-lg font-bold">Game Modes</h2>
-          <p className="text-primary font-semibold">
-            {game.gameModes?.length > 0
-              ? game.gameModes.map((mode) => mode.name).join(", ")
-              : "N/A"}
-          </p>
-        </div>
-
-        <div className="mb-5">
-          <h2 className="mb-1 text-lg font-semibold">Age Ratings</h2>
-          <ul className="grid grid-cols-3">
-            {game.ageRatings?.length > 0
-              ? game.ageRatings.map((rating) => (
-                  <li className="text-dark font-semibold" key={rating.id}>
-                    {rating.organization.name}
-                  </li>
-                ))
-              : "N/A"}
-          </ul>
-        </div>
-      </div>
-      <div className="mt-5 flex items-center justify-center lg:hidden">
-        <a href="#">Back to Top</a>
       </div>
     </div>
   );
