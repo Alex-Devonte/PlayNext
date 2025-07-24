23289661195ce765380a9a58a37846454dee6d4b
Alex <alex95.taylor@gmail.com>
2025-07-24 17:59:29 -0400

    Update routes to handle 404 errors
    Catch-all route will navigate to a 404 page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Footer from "./components/Footer";
 import GameDetail from "./components/GameDetail";
 import Questionnaire from "./components/Questionnaire";
 import Results from "./components/Results";
 import Header from "./components/Header";
+import PageNotFound from "./components/PageNotFound";
 
 function App() {
   return (
@@ -17,6 +23,8 @@ function App() {
             <Route path="/questionnaire" element={<Questionnaire />} />
             <Route path="/game/:id" element={<GameDetail />} />
             <Route path="/results" element={<Results />} />
+            <Route path="/404" element={<PageNotFound />} />
+            <Route path="*" element={<Navigate to="/404" replace />} />
           </Routes>
         </main>
         <Footer />
