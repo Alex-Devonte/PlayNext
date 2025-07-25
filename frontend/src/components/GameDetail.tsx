0270c21049afb37692a5deed60234916dfcb9521
Alex <alex95.taylor@gmail.com>
2025-07-25 18:43:33 -0400

    Change 'back to top' link to animated SVG icon

--- frontend/src/components/GameDetail.tsx
+++ frontend/src/components/GameDetail.tsx
@@ -220,8 +220,24 @@ function GameDetail() {
             </ul>
           </div>
         </div>
-        <div className="mt-5 flex items-center justify-center lg:hidden">
-          <a href="#">Back to Top</a>
+        <div className="lg:hidden">
+          <a href="#">
+            <svg
+              className="animate-smallBounce absolute right-5 size-14"
+              fill="#FED766"
+              version="1.1"
+              viewBox="-4 -4 48.00 48.00"
+              stroke="#FED766"
+            >
+              <g stroke-width="0"></g>
+              <g stroke-linecap="round" stroke-linejoin="round"></g>
+              <g>
+                <g>
+                  <path d="M20,0C8.973,0,0,8.973,0,20c0,11.027,8.973,20,20,20c11.027,0,20-8.973,20-20C40,8.973,31.027,0,20,0z M28.018,19.752 c-0.451,0.531-1.094,0.805-1.738,0.805c-0.521,0-1.047-0.179-1.477-0.543l-2.521-2.141v9.186c0,1.26-1.021,2.28-2.281,2.28 c-1.26,0-2.281-1.021-2.281-2.28v-9.186l-2.521,2.141c-0.96,0.816-2.4,0.698-3.216-0.262c-0.816-0.961-0.698-2.4,0.262-3.216 l6.279-5.333c0.852-0.723,2.102-0.723,2.955,0l6.278,5.333C28.716,17.352,28.834,18.791,28.018,19.752z"></path>{" "}
+                </g>
+              </g>
+            </svg>
+          </a>
         </div>
       </div>
     </div>
